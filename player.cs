3e7144547ece8979d3fe8a44296c39b51af7f8ad
Brushey165732 <165732@tldsb.me>
2018-05-17 15:12:37 -0400

    Update player.cs

--- player.cs
+++ player.cs
@@ -13,17 +13,27 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Drawing;
 
 namespace Donkey_Kong
 {
     class Player
     {
+        int top_1;
+        int bottom_1;
+        int top_2;
+        int bottom_2;
+        int top_3;
+        int bottom_3;
+        int top_4;
+        int bottom_4;
+
         int y_vel = 0;
         Window window;
         Canvas canvas;
         bool playerisgenerated = false;
 
-        Point point = new Point(200,200);
+        Point point = new Point(700,80);
 
         public Player(Window w, Canvas c)
         {
@@ -46,7 +56,71 @@ namespace Donkey_Kong
                         point.X += 5;
                     }
                 }
-                if ((point.Y < 500 && point.Y > 380) || (point.Y < 240 && point.Y > 165))
+                else if (point.X <= 750 && point.X >= 149 && point.Y + 50 <= 193 && point.Y + 50 >= 139)
+                {
+                    if (point.Y + 50 >= -0.05 * (point.X) + 172 && point.Y + 50 <= point.X + 176)
+                    {
+                        if (Keyboard.IsKeyDown(Key.Left))
+                        {
+                            point.X -= 4.75;
+                            point.Y += 0.25;
+                        }
+                        if (Keyboard.IsKeyDown(Key.Right))
+                        {
+                            point.X += 4.75;
+                            point.Y -= 0.25;
+                        }
+                    }
+                }
+                else if (point.X <= 624 && point.X >= 22 && point.Y + 50 <= 301 && point.Y + 50 >= 245)
+                {
+                    if (point.Y + 50 >= 0.05 * (point.X) + 245 && point.Y + 50 <= 0.05 * (point.X) + 249)
+                    {
+                        if (Keyboard.IsKeyDown(Key.Left))
+                        {
+                            point.X -= 4.75;
+                            point.Y -= 0.25;
+                        }
+                        if (Keyboard.IsKeyDown(Key.Right))
+                        {
+                            point.X += 4.75;
+                            point.Y += 0.25;
+                        }
+                    }
+                }
+                else if (point.X <= 750 && point.X >= 149 && point.Y + 50 <= 421 && point.Y + 50 >= 367)
+                {
+                    if (point.Y + 50 >= -0.05 * (point.X) + 398 && point.Y + 50 <= 0.05 * (point.X) + 402)
+                    {
+                        if (Keyboard.IsKeyDown(Key.Left))
+                        {
+                            point.X -= 4.75;
+                            point.Y += 0.25;
+                        }
+                        if (Keyboard.IsKeyDown(Key.Right))
+                        {
+                            point.X += 4.75;
+                            point.Y -= 0.25;
+                        }
+                    }
+                }
+                else if (point.X <= 624 && point.X >= 22 && point.Y + 50 <= 551 && point.Y + 50 >= 496)
+                {
+                    if (point.Y + 50 >= 0.05 * (point.X) + 496 && point.Y + 50 <= 0.05 * (point.X) + 500)
+                    {
+                        if (Keyboard.IsKeyDown(Key.Left))
+                        {
+                            point.X -= 4.75;
+                            point.Y -= 0.25;
+                        }
+                        if (Keyboard.IsKeyDown(Key.Right))
+                        {
+                            point.X += 4.75;
+                            point.Y += 0.25;
+                        }
+                    }
+                }
+                /*if ((point.Y < 500 && point.Y > 380) || (point.Y < 240 && point.Y > 165))
                 {
                     if (Keyboard.IsKeyDown(Key.Left))
                     {
@@ -71,28 +145,99 @@ namespace Donkey_Kong
                         point.X += 4.75;
                         point.Y -= 0.25;
                     }
+                }*/
+                else
+                {
+                    if (Keyboard.IsKeyDown(Key.Left))
+                    {
+                        point.X -= 1;
+                    }
+                    if (Keyboard.IsKeyDown(Key.Right))
+                    {
+                        point.X += 1;
+
+                    }
                 }
-                
-            }
-            
-           
+            }        
         }
         public void fall()
         {
+            
             if (point.Y == 590)
             {
                 y_vel = 0;
+                Console.WriteLine("in sector 0");
             }
             else if (point.Y > 590)
             {
-                point.Y = 640;
+                point.Y = 590;
                 y_vel = 0;
+                Console.WriteLine("in sector 0");
             }
-            else if ((point.X > 635) || (point.Y > 230 && point.Y < 270))
+            else if (point.X <= 750 && point.X >= 149 && point.Y + 50 <= 193 && point.Y + 50 >= 139)
             {
-                point.Y += 3;
-                point.X += 0.25;
+                if (point.Y + 50 >= -0.05 * (point.X) + 172 && point.Y + 50 <= point.X + 176)
+                {
+                    y_vel = 0;
+                    Console.WriteLine("in sector 1");
+                }
+                else
+                {
+                    y_vel = 3;
+                    Console.WriteLine("falling");
+                }
             }
+            //second from top
+            else if (point.X <= 624 && point.X >= 22 && point.Y + 50 <= 301 && point.Y + 50 >= 245)
+            {
+                if (point.Y + 50 >= 0.05 * (point.X) + 245 && point.Y + 50 <= 0.05 * (point.X) + 249)
+                {
+                    y_vel = 0;
+
+                    Console.WriteLine("in sector 2");
+                }
+                else
+                {
+                    y_vel = 3;
+                    Console.WriteLine("falling");
+                }
+            }
+            //second from bottom
+            else if (point.X <= 750 && point.X >= 149 && point.Y + 50 <= 421 && point.X + 50 >= 365)
+            {
+                if (point.Y + 50 >= -0.05 * (point.X) + 398 && point.Y + 50 <= 0.05 * (point.X) + 402)
+                {
+                    y_vel = 0;
+                    Console.WriteLine("in sector 3");
+                }
+                else
+                {
+                    y_vel = 3;
+                    Console.WriteLine("falling");
+                }
+            }
+            //lowest angled bar
+            else if (point.X <= 624 && point.X >= 22 && point.Y + 50 <= 551 && point.Y + 50 >= 496)
+            {
+                if (point.Y + 50 >= 0.05 * (point.X) + 496 && point.Y + 50 <= 0.05 * (point.X) + 500)
+                {
+                    y_vel = 0;
+                    Console.WriteLine("in sector 4");
+                }
+                else
+                {
+                    y_vel = 3;
+                    Console.WriteLine("falling");
+                }
+            }
+            else
+            {
+                y_vel = 3;
+                Console.WriteLine("falling");
+            }
+
+            point.Y += y_vel;
+
         }
         public void jump()
         {
@@ -101,7 +246,7 @@ namespace Donkey_Kong
             {
                 for (int i = 0; i <= 5; i++)
                 {
-
+                    
                 }
             }
         }
@@ -119,18 +264,13 @@ namespace Donkey_Kong
             Canvas.SetTop(player_sprite, point.Y);
         }
         public void generate(Rectangle player_sprite)
-        {
-            
-            
+        {         
             player_sprite.Width = 25;
             player_sprite.Height = 50;
             player_sprite.Fill = Brushes.White;
             Canvas.SetLeft(player_sprite, point.X);
             Canvas.SetTop(player_sprite, point.Y);
-            
-
-
-            
+                      
         }
     }
 }
